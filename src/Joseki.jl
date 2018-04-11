646c84e2f88dbeab5aedb08c0cb969b69c5de17a
Alex Mellnik <a.r.mellnik@gmail.com>
2018-04-10 20:31:37 -0700

    Middleware modifies request, error handlers get requests
    Let's make it clear that we're just passing around a single request that we are modifying (along with the response it contains).  Also allow error handlers to access partially-formed requests and responses.

--- src/Joseki.jl
+++ src/Joseki.jl
@@ -11,27 +11,27 @@ include("middleware.jl")
 
 export stack
 """
-    stack(fns::Array{Function, 1}, endpoint::Function; error_fn=rethrow)
+    stack(fns::Array{Function, 1}, endpoint::Function; error_fn=(req, err)->rethrow(err))
 
 Create a `HTTP.Handlers.HandlerFunction` from a set of middlware functions (of
 the form `HTTP.Request` => `HTTP.Request`) and an endpoint (of the form
 `HTTP.Request` => `HTTP.Response`).  If `error_fn` is not provided then any
-requests that cause an error respond with the default HTTP.jl rendering of that
-error.
+requests that cause an unhandled error respond with the default HTTP.jl
+rendering of that error (a plain text string).
 """
 function stack(fns::Array{Function, 1}, endpoint::Function;
-               error_fn=rethrow)
+               error_fn=(req, err)->rethrow(err))
     # First we form the function that represents our middleware + endpoint
     # wrapped in a try-catch block so that we can deal with errors that are not
     # explicitly handled.
     s = function(req::HTTP.Request)
         try
-            for f in fns
-                req = f(req)
+            for f! in fns
+                f!(req)
             end
             return endpoint(req)
         catch err
-            error_fn(err)
+            error_fn(req, err)
         end
     end
     # Once we have this function we wrap it in a HandlerFunction which is used
--- src/middleware.jl
+++ src/middleware.jl
@@ -5,7 +5,7 @@ This sets `Access-Control-Allow-Origin` to the requester's origin.  In
 production you would want to check the origin against a whitelist and throw an
 error otherwise.
 """
-function add_cors(req::HTTP.Request)
+function add_cors!(req::HTTP.Request)
     orig = HTTP.Messages.header(req, "Origin")
     push!(req.response.headers, Pair("Access-Control-Allow-Origin", orig))
     push!(req.response.headers, Pair("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS"))
@@ -15,7 +15,7 @@ end
 """
 Sets `Content-Type` for JSON responses.
 """
-function content_type(req::HTTP.Request)
+function content_type!(req::HTTP.Request)
     push!(req.response.headers,
         Pair("Content-Type", "application/json; charset=utf-8"))
     return req
@@ -24,7 +24,7 @@ end
 """
 Log a line whenever you get a request.  HTTP.jl already does this.
 """
-function hit_logger(req::HTTP.Request)
+function hit_logger!(req::HTTP.Request)
     println(req.method, " request to: ", req.target,
         " at ", Dates.now())
     return req
@@ -33,10 +33,10 @@ end
 """
 Log the request body.
 """
-function body_logger(req::HTTP.Request)
+function body_logger!(req::HTTP.Request)
     println(String(req.body))
     return req
 end
 
 # Some default middleware
-default_middleware = [hit_logger, body_logger, add_cors, content_type]
+default_middleware = [hit_logger!, body_logger!, add_cors!, content_type!]
