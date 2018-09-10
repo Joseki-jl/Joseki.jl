5894e6df3fdfe30a60a4f5437e9bf1e745b874f5
Alex Mellnik <alex.mellnik@rigetti.com>
2018-09-10 11:35:52 -0700

    Add a JSON responder that just returns the object
    Also provide a way to easily generate 500 code error responses.

--- src/responders.jl
+++ src/responders.jl
@@ -21,6 +21,21 @@ function json_responder(req::HTTP.Request, o::T) where {T<:Union{Number, Abstrac
     return req.response
 end
 
+
+export simple_json_responder
+"""
+    simple_json_responder(req::HTTP.Request, o)
+
+Respond simply with the JSON serialization of an object.  `o` can be a `Number`, `String`,
+`Dict`, or an `Array` of one of the other types.
+"""
+function simple_json_responder(req::HTTP.Request, o::T) where {T<:Union{Number, AbstractString, Array, Dict}}
+    b = JSON.json(o)
+    req.response.body = bytes(b)
+    return req.response
+end
+
+
 export error_responder
 """
     error_responder(req::HTTP.Request, e::String)
@@ -40,10 +55,10 @@ The response will look like:
 ```
 """
 function error_responder(req::HTTP.Request, e::String)
-    
+
     # Endpoints can do their own error checking.  Errors caught by them
     # return a 200-code JSON response that has a key 'error' set to true
-    
+
     # Make sure we don't have unescaped quotes in the error string since
     # we are going to return it as a json
     ee = replace(e, r"(?<!\\)(?:\\{2})*\K\"" => "\\\"")
@@ -62,7 +77,20 @@ error_responder(req::HTTP.Request, e::Exception) =
 
 The default error handler, not intended for use by users.  It includes a gentle reminder that errors should typically be caught elsewhere.
 """
-function unhandled_error_responder(req::HTTP.Request,e::Exception)
-    println("Unhandled error!  You probably want to catch these: ", string(e))
+function unhandled_error_responder(req::HTTP.Request, e::Exception)
+    @info string("Unhandled error!  You probably want to catch these: ", string(e))
     error_responder(req, string(e))
+end
+
+
+export critical_error_responder
+"""
+    critical_error_responder(req::HTTP.Request,e::Exception)
+
+Respond with a 500 error response.
+"""
+function critical_error_responder(req::HTTP.Request, e::Exception)
+    @warn string("Critical error!  Returning a 500 response to the client: ", string(e))
+    setstatus(req.response, 500)
+    return req.response
 end
