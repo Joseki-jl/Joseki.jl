f8ce2b4c051f850ba1b053758581b9bbd391bbec
Alex Mellnik <a.r.mellnik@gmail.com>
2019-10-09 20:54:43 -0700

    Try running docker-based tests in Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 # These tests are closely based on the server tests in HTTP.jl:
 # https://github.com/JuliaWeb/HTTP.jl/blob/master/test/server.jl
 
-
 include("integration-tests.jl")
 include("utilities-tests.jl")
-# include("dockerfile-tests.jl")
+include("dockerfile-tests.jl")
