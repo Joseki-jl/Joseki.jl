d446b227208363824748652dc25e15775beb5c58
Alex Mellnik <a.r.mellnik@gmail.com>
2019-10-21 17:57:30 -0700

    Add test coverage for this bug

--- test/integration-tests.jl
+++ test/integration-tests.jl
@@ -14,10 +14,15 @@ using HTTP, Joseki, JSON, Sockets, Test
         json_responder(req, x^y)
     end
 
+    function test_error_responder(req::HTTP.Request, e::Exception)
+        error_responder(req, "Unhandled error!")
+    end
+
     endpoints = [
         (pow, "GET", "/pow")
     ]
-    router = Joseki.router(endpoints) # Default middleware
+    # Default middleware but a custom error responder
+    router = Joseki.router(endpoints; error_fn=test_error_responder)
 
     @info "Starting test server"
     server = Sockets.listen(Sockets.InetAddr(ip"127.0.0.1", 8000))
@@ -25,9 +30,15 @@ using HTTP, Joseki, JSON, Sockets, Test
                                     server = server)
     sleep(1.0)
 
+    # A valid request
     res = HTTP.get("http://localhost:8000/pow/?x=2&y=3")
     @test res.status == 200
     @test HTTP.hasheader(res, "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
+
+    # An invalid request
+    res = HTTP.get("http://localhost:8000/pow/?x=2&y=nothing")
+    @test res.status == 200 # Our handeled errors return 200 responses
+    @test occursin("Unhandled error!", String(copy(res.body)))
 
     @info "Shutting down test server"
 
